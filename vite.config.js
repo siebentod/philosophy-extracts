a65f431d1863504f7b10a331773c35fc161ed3d8
siebentod <33163165+siebentod@users.noreply.github.com>
2024-07-22 08:58:27 +0300

    small

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import Sitemap from 'vite-plugin-sitemap';
+import { data } from './src/data';
+
+const dynamicRoutes = ['/', '/about', ...data.map((ar) => `/${ar.id}`)];
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    Sitemap({
+      hostname: 'https://philosophy-extracts.vercel.app/',
+      dynamicRoutes,
+    }),
+  ],
+});
